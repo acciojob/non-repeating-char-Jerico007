9bee7f072ff458fd24b424f4c06bdfa054cd0d01
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-07-13 21:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 //your JS code here. If required.
-  // let a = prompt("Enter a string");
+  let a = prompt("Enter a string");
 
-   let arr = [...a];
 
-	arr.sort();
+  let arr = a.split("");
 
-  a = arr.join();
+      arr.sort();
+
+      a = arr.join("");
+      console.log(a);
 
       let ans = function NonReapeating(val) {
         let finalAns = val.charAt(0);
-        let index = 1;
-         while(index < val.length) {
-          let j = index+1;
+        
+        let index = 0;
+        while (index < val.length) {
+         
+          let j = index + 1;
           let cnt = 1;
-          while(j < val.length)
-          {
-            if(val.charAt(j) === val.charAt(index))
-            {
-               cnt++;
-               j++;
-            }
-            else{
+          
+          while (j < val.length) {
+            if (val.charAt(j) === val.charAt(index)) {
+              cnt++;
+            } else {
               break;
             }
+            j++;
           }
 
-          if(cnt === 1)
-          {
+          if (cnt === 1) {
             finalAns = val.charAt(index);
-			  break;
-          }
-          else{
+            break;
+          } else {
             index = j;
-          } 
+          }
         }
         return finalAns;
       };
 
-     // alert(ans(a));
+
+     alert(ans(a));
+

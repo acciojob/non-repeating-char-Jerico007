202cd82968e14b3801e3e346bbc33e881c4c9c0a
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-07-13 20:13:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+ let a = prompt("Enter a string");
+      
+      let ans = function NonReapeating(val) {
+        let finalAns = val.charAt(0);
+        for (let index = 0; index < val.length-1; index++ ) {
+          let j = index+1;
+          while(j < val.length)
+          {
+            if(val.charAt(j) === val.charAt(index))
+            {
+              j++;
+            }
+            else{
+              finalAns = val.charAt(index);
+              index = j;
+              break;
+            }
+          }
+        }
+        return finalAns;
+      };
+
+     alert(ans(a));
